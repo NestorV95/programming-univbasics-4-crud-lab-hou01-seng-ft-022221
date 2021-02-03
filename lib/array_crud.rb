46f4a31e8ae384d050ed254aa174f3144f20768b
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-03 18:17:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,33 +17,3 @@ element = "wow"
  array.unshift element
 end 
 
-def remove_element_from_end_of_array(array)
- ["I", "am", "really", "learning", "arrays!"]
-  array.Pop
-end
-
-def remove_element_from_start_of_array(array)
-["wow", "I", "am", "really", "learning", "arrays!"]
-  array.Shift
-end
-
-def retrieve_element_from_index(array, index number)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
-index_number = 2
-array[index_number]
-end
-
-def retrieve_first_element_from_array(array)
-["wow", "I", "am", "really", "learning", "arrays!"]
-array)[0]
-end
-
-def retrieve_last_element_from_array(array)
-["wow", "I", "am", "really", "learning", "arrays!"]
-array[5]
-end
-
-def update_element_from_index(array)
-["wow", "I", "am", "really", "learning", "arrays!"]
-array[4] = "Totally"
-end
