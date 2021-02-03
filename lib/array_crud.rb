ffac47ad15a48129fd3efc0b4b87493c7baa09bb
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-03 18:19:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,6 +18,6 @@ element = "wow"
 end 
 
 def remove_element_from_end_of_array(array)
- ["I", "am", "really", "learning", "arrays!"]
+ array = ["I", "am", "really", "learning", "arrays!"]
   array.Pop
 end
