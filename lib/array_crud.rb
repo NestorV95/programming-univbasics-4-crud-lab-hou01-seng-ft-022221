b57ccee1f3a6414c1dc6cdd00df46b20a61950c5
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-03 18:21:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,17 +33,3 @@ index_number = 2
 array[index_number]
 end
 
-def retrieve_first_element_from_array(array)
-["wow", "I", "am", "really", "learning", "arrays!"]
-array)[0]
-end
-
-def retrieve_last_element_from_array(array)
-["wow", "I", "am", "really", "learning", "arrays!"]
-array[5]
-end
-
-def update_element_from_index(array)
-["wow", "I", "am", "really", "learning", "arrays!"]
-array[4] = "Totally"
-end
